e18a098895b75080a4b6b62621ef9f98927aaf85
THQ4 <96069634+THQ4@users.noreply.github.com>
2021-12-13 23:06:01 +0700

    Add files via upload

---
+++ minergate_ethminer_opencl-eth_eth.bat
@@ -0,0 +1,6 @@
+set GPU_FORCE_64BIT_PTR=0
+set GPU_MAX_HEAP_SIZE=100
+set GPU_USE_SYNC_OBJECTS=1
+set GPU_MAX_ALLOC_PERCENT=100
+set GPU_SINGLE_ALLOC_PERCENT=100
+ethminer -G -P stratum+tcp://eth.pool.minergate.com:45791/fxtwenty4@gmail.com
